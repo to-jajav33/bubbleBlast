e108a0eaf263ab2a21596b1266df856b092352dc
jajav33 <to.jajav33@gmail.com>
2020-07-27 20:59:01 -0700

    added more animations

---
+++ src/components/Player.gd
@@ -0,0 +1,30 @@
+extends AnimatedSprite
+
+
+# Declare member variables here. Examples:
+export var animationFPS = 5.0;
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	set_process(true);
+	pass; # Replace with function body.
+
+func _process(delta):
+	if (Input.is_action_pressed("left")):
+		get_node(".").play("walk_left");
+	elif (Input.is_action_pressed("right")):
+		get_node(".").play("walk_right");
+	elif (Input.is_action_pressed("up")):
+		get_node(".").play("walk_up");
+	elif (Input.is_action_pressed("down")):
+		get_node(".").play("walk_down");
+	else:
+		get_node(".").stop();
+	
+	frames.set_animation_speed(animation, animationFPS);
+	pass;
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
