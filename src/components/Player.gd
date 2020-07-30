0bb8a597d9bb6ef3239abe99e9a1d5656a375b23
jajav33 <to.jajav33@gmail.com>
2020-07-29 21:46:49 -0700

    got attack animation going

--- src/components/Player.gd
+++ src/components/Player.gd
@@ -3,6 +3,9 @@ extends AnimatedSprite
 
 # Declare member variables here. Examples:
 export var animationFPS = 5.0;
+export var speed = 100.0;
+var oldDir = Vector2.LEFT;
+var isFinished = true;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -10,16 +13,34 @@ func _ready():
 	pass; # Replace with function body.
 
 func _process(delta):
-	if (Input.is_action_pressed("left")):
-		get_node(".").play("walk_left");
-	elif (Input.is_action_pressed("right")):
-		get_node(".").play("walk_right");
-	elif (Input.is_action_pressed("up")):
-		get_node(".").play("walk_up");
-	elif (Input.is_action_pressed("down")):
-		get_node(".").play("walk_down");
-	else:
-		get_node(".").stop();
+	var dir = Vector2.ZERO;
+	var isAttacking = (Input.is_action_just_pressed("attack")) || ((isFinished == false) && (animation == "attack_right" || animation == "attack_left"));
+	
+	if (Input.is_action_just_pressed("attack")):
+		isFinished = false;
+		if (oldDir == Vector2.LEFT):
+			get_node(".").play("attack_left");
+		elif (oldDir == Vector2.RIGHT):
+			get_node(".").play("attack_right");
+	
+	if (isAttacking == false):
+		if (Input.is_action_pressed("left")):
+			dir += Vector2.LEFT;
+		
+		if (Input.is_action_pressed("right")):
+			dir += Vector2.RIGHT;
+		
+		if (dir == Vector2.LEFT):
+			get_node(".").play("walk_left");
+			oldDir = dir;
+		elif (dir == Vector2.RIGHT):
+			get_node(".").play("walk_right");
+			oldDir = dir;
+		else:
+			get_node(".").stop();
+			get_node(".").frame = 0;
+	
+		position = position + (dir * speed * delta);
 	
 	frames.set_animation_speed(animation, animationFPS);
 	pass;
@@ -28,3 +49,10 @@ func _process(delta):
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+
+func _on_Player_animation_finished():
+	isFinished = true;
+	get_node(".").frame = 0;
+	get_node(".").stop();
+	pass # Replace with function body.
